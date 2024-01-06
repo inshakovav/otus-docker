ac17d890386ca8b37b970f0151576742df55378b
Aleksey Inshakov <aleksey.inshakov@domrf.ru>
2024-01-06 06:33:10 +0300

    Add Sprig Boot app with REST controller

---
+++ src/main/java/com/example/testapp/Controller.java
@@ -0,0 +1,13 @@
+package com.example.testapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/health")
+    String getStatus() {
+        return "All is OK";
+    }
+}
